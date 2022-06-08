de9c167f93be39a391c429a45f78c1f5a8b974d9
dannylo-gaivota <80546050+dannylo-gaivota@users.noreply.github.com>
2022-06-08 19:25:58 -0300

    feat: added post and list routes

---
+++ web/handlers/coin.go
@@ -0,0 +1,66 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/codegangsta/negroni"
+	"github.com/eudangeld/clean_go/core/coin"
+	"github.com/gorilla/mux"
+)
+
+func MakeCoinHandlers(r *mux.Router, n *negroni.Negroni, service coin.UseCase) {
+	r.Handle("/v1/coin", n.With(
+		negroni.Wrap(getAllCoin(service)),
+	)).Methods("GET", "OPTIONS")
+
+	r.Handle("/v1/coin", n.With(
+		negroni.Wrap(insertCoin(service)),
+	)).Methods("POST", "OPTIONS")
+}
+
+
+
+func getAllCoin(service coin.UseCase) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+			
+			
+			w.Header().Set("Content-Type", "application/json")
+			all, err := service.GetAll()
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			
+			err = json.NewEncoder(w).Encode(all)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+		})
+}
+
+
+func insertCoin(service coin.UseCase) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			fmt.Println("Post new coin")
+			var coin coin.Coin
+
+			err:= json.NewDecoder(r.Body).Decode(&coin)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+			}
+			err = service.Store(&coin);
+
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			w.WriteHeader(http.StatusCreated)
+		})
+}
+
+
--- web/main.go
+++ web/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -10,6 +9,7 @@ import (
 
 	"github.com/codegangsta/negroni"
 	"github.com/eudangeld/clean_go/core/coin"
+	"github.com/eudangeld/clean_go/web/handlers"
 	"github.com/gorilla/mux"
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -29,10 +29,11 @@ func main () {
 	r:= mux.NewRouter()
 	n := negroni.New(negroni.NewLogger())
 
-	r.Handle("/v1/coin", n.With(
-		negroni.Wrap(coinHandler(service)),
-	)).Methods("GET", "OPTIONS")
+	
+	//init handler for cois
+	handlers.MakeCoinHandlers(r, n, service)
 	http.Handle("/", r)
+
 	srv := &http.Server{
 		ReadTimeout: 30* time.Second,
 		WriteTimeout: 30* time.Second,
@@ -48,12 +49,3 @@ func main () {
   
 }
 
-
-func coinHandler(service coin.UseCase) http.Handler {
-	return http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {
-		books, _ := service.GetAll()
-		for _, b := range books {
-			fmt.Println(b)
-		}
-	})
-}
